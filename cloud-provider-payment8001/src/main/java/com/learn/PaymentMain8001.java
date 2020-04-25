059dd753218c6ebcaf243c2d10fa8a24f0af1e91
jusang <735409556@qq.com>
2020-04-25 15:10:04 +0800

    集群化的其他修改

--- cloud-provider-payment8001/src/main/java/com/learn/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/learn/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author :   ''
@@ -20,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Why & What is modified: <修改原因描述>
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
