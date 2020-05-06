77f99ae7049ccc3d08b4f8c55a35dba85072092f
jusang <735409556@qq.com>
2020-05-06 21:18:30 +0800

    添加默认的服务降级方法

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/learn/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,9 @@
 package com.learn.springcloud.controller;
 
 import com.learn.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "paymentInfo_GlobalErrorHandler")
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -22,6 +26,10 @@ public class OrderHystrixController {
         return result;
     }
 
+//    @HystrixCommand(fallbackMethod = "paymentInfo_ErrorHandler",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "9000")
+//    })
+    @HystrixCommand //使用服务降级，但是不指明
     @GetMapping("consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_Timeout (@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_Timeout(id);
@@ -29,4 +37,18 @@ public class OrderHystrixController {
         return result;
     }
 
+    public String paymentInfo_ErrorHandler (@PathVariable("id") Integer id) {
+        String result = "等一下";
+        log.info(result);
+        return result;
+    }
+
+
+    // 全局fallback
+    // 这里不能在方法里添加参数，否则无法找到该方法
+    public String paymentInfo_GlobalErrorHandler () {
+        String result = "我是全局处理";
+        log.info(result);
+        return result;
+    }
 }
