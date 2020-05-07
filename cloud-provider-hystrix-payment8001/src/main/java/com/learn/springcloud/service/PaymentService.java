b4ee644f7bde7565618278a95b4b5ceb2030357c
jusang <735409556@qq.com>
2020-05-07 20:08:01 +0800

    。。

--- cloud-provider-hystrix-payment8001/src/main/java/com/learn/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/learn/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.learn.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -12,13 +14,24 @@ public class PaymentService {
     }
 
     // 错误的方法
+    // 使用注解添加服务操作fallback
+    @HystrixCommand(fallbackMethod = "paymentInfo_ErrorHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_Error(Integer id){
         try {
-            TimeUnit.SECONDS.sleep(3);
+            // 延长时间
+            TimeUnit.SECONDS.sleep(2);
+//            TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程池" + Thread.currentThread().getName() + "\tpaymentInfo_tiemout\t" + id;
+    }
+
+    // 兜底方法
+    public String paymentInfo_ErrorHandler(Integer id){
+        return "线程池" + Thread.currentThread().getName() + "\tpaymentInfo_ErrorHandler\t" + id;
     }
 
 }
